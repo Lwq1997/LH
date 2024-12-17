5a158c11a8deaca1e65e59d150e0b26f59ebb314
liwanqi.gold <liwanqi.gold@bytedance.com>
2024-12-17 23:09:20 +0800

    新增代码

---
+++ DXW_Strategy.py
@@ -0,0 +1,312 @@
+# -*- coding: utf-8 -*-
+# 如果你的文件包含中文, 请在文件的第一行使用上面的语句指定你的文件编码
+
+# 用到策略及数据相关API请加入下面的语句(如果要兼容研究使用可以使用 try except导入
+import datetime
+import math
+import numpy as np
+import pandas as pd
+import pickle
+import requests
+import talib
+
+from kuanke.user_space_api import *
+from kuanke.wizard import *
+from jqdata import *
+from jqfactor import *
+from jqlib.technical_analysis import *
+from scipy.linalg import inv
+from prettytable import PrettyTable
+from WPETF_Strategy import WPETF_Strategy
+from XSZ_GJT_Strategy import XSZ_GJT_Strategy
+from Strategy import Strategy
+
+import warnings
+import datetime as dt
+
+
+# DSZMX策略
+class DXW_Strategy(Strategy):
+    def __init__(self, context, subportfolio_index, name, params):
+        super().__init__(context, subportfolio_index, name, params)
+        self.new_days = 375  # 已上市天数
+        self.no_trading_today_signal = False
+        self.market_temperature = "warm"
+        self.highest = 50
+        self.buy_stock_count = 5
+        self.singal_str = 'Unknown'
+        self.ETF_pool = []
+        self.B_stocks = []
+        self.S_stocks = []
+        self.foreign_ETF = [
+            '518880.XSHG',
+            '513030.XSHG',
+            '513100.XSHG',
+            '164824.XSHE',
+            '159866.XSHE',
+        ]
+
+    def singal(self, context):
+        dt_last = context.previous_date
+        log.info(self.name, '--singal函数开始运行--',
+                    str(context.current_dt.date()) + ' ' + str(context.current_dt.time()))
+
+        self.B_stocks = self.stockpool_index(context, '000300.XSHG')
+        # 过滤次新股
+        self.B_stocks = self.utilstool.filter_new_stock(context, self.B_stocks, self.new_days)
+        self.S_stocks = self.stockpool_index(context, '399101.XSHE')
+        # 过滤次新股
+        self.S_stocks = self.utilstool.filter_new_stock(context, self.S_stocks, self.new_days)
+
+        self.select_list = self.__get_rank(context)[:self.max_select_count]
+        self.print_trade_plan(context, self.select_list)
+
+        q = query(
+            valuation.code, valuation.circulating_market_cap
+        ).filter(
+            valuation.code.in_(self.B_stocks)
+        ).order_by(
+            valuation.circulating_market_cap.desc()
+        )
+        df = get_fundamentals(q)
+        Blst = list(df.code)[:20]
+
+        q = query(
+            valuation.code, valuation.circulating_market_cap
+        ).filter(
+            valuation.code.in_(self.S_stocks)
+        ).order_by(
+            valuation.circulating_market_cap.asc()
+        )
+        df = get_fundamentals(q, date=dt_last)
+        Slst = list(df.code)[:20]
+
+        B_ratio = get_price(Blst, end_date=dt_last, frequency='1d', fields=['close'], count=10, panel=False
+                            ).pivot(index='time', columns='code', values='close')
+        change_BIG = (B_ratio.iloc[-1] / B_ratio.iloc[0] - 1) * 100
+        A1 = np.array(change_BIG)
+        A1 = np.nan_to_num(A1)
+        B_mean = np.mean(A1)
+
+        S_ratio = get_price(Slst, end_date=dt_last, frequency='1d', fields=['close'], count=10, panel=False
+                            ).pivot(index='time', columns='code', values='close')
+        change_SMALL = (S_ratio.iloc[-1] / S_ratio.iloc[0] - 1) * 100
+        A1 = np.array(change_SMALL)
+        A1 = np.nan_to_num(A1)
+        S_mean = np.mean(A1)
+
+        if B_mean > S_mean and B_mean > 0:
+            if B_mean > 5:
+                self.singal_str = 'small'
+            else:
+                self.singal_str = 'big'
+        elif B_mean < S_mean and S_mean > 0:
+            self.singal_str = 'small'
+        else:
+            self.singal_str = 'etf'
+            deltaday = 20
+            self.ETF_pool = self.fun_delNewShare(context, self.foreign_ETF, deltaday)
+            if len(self.ETF_pool) == 0:
+                self.ETF_pool = self.fun_delNewShare(context, ['511010.XSHG'], deltaday)
+
+        log.info(self.name, '--singal函数运行完成，运行后的结果：', self.singal_str, '--now--',
+                 str(context.current_dt.date()) + ' ' + str(context.current_dt.time()))
+
+        # if B_mean > 10 or S_mean > 10:
+        #     print('无敌好行情')
+        #     if B_mean > S_mean:
+        #         print('开大')
+        #         choice = B_stocks
+        #         target_list1 = ROIC_BIG(context, choice)
+        #         target_list2 = BIG(context, choice)
+        #         target_list3 = BM(context, choice)
+        #         target_list = target_list3 + target_list1 + target_list2
+        #         target_list = list(set(target_list))
+        #     else:
+        #         print('开小')
+        #         choice = S_stocks
+        #         target_list = SMALL(context, choice)[:self.stock_num * 3]
+        # elif B_mean > S_mean and B_mean > 0:
+        #     print('开大')
+        #     choice = B_stocks
+        #     target_list2 = ROIC_BIG(context, choice)
+        #     target_list1 = BIG(context, choice)
+        #     target_list3 = BM(context, choice)
+        #     target_list = target_list1 + target_list2 + target_list3
+        #     target_list = list(set(target_list))
+        #
+        # elif B_mean < S_mean and S_mean > 0:
+        #     print('开小')
+        #     choice = S_stocks
+        #     target_list = SMALL(context, choice)[:self.stock_num * 3]
+        # else:
+        #     print('开外盘')
+        #     target_list = self.foreign_ETF
+
+    def select(self, context):
+        self.select_list = self.__get_rank(context)[:self.max_select_count]
+        self.print_trade_plan(context, self.select_list)
+
+    def __get_rank(self, context):
+
+        log.info(self.name, '--get_rank函数--', str(context.current_dt.date()) + ' ' + str(context.current_dt.time()))
+
+        target_list = []
+
+        if self.singal_str == 'big':
+            target_list = self.White_Horse(context)
+        elif self.singal_str == 'small':
+            target_list = self.SMALL(context)
+        elif self.singal_str == 'etf':
+            # target_list = self.foreign_ETF
+            target_list = self.ETF_pool
+        return target_list
+
+    ## 开盘前运行函数
+    def White_Horse(self, context):
+        self.market_temperature = self.utilstool.Market_temperature(context)
+
+        all_stocks = self.B_stocks
+        if self.market_temperature == "cold":
+            q = query(
+                valuation.code,
+            ).filter(
+                # TODO
+                # valuation.pb_ratio > 0,
+                # valuation.pb_ratio < 1,
+                # cash_flow.subtotal_operate_cash_inflow > 0,  # 经营活动现金流入小计
+                # indicator.adjusted_profit > 2.5e8,  # 扣除非经常损益后的净利润(元)                          #>=2.5亿
+                # income.operating_revenue > 10e8,  # 营业收入(元)                                          #>=10亿
+                # income.net_profit > 2.5e8,  # 净利润(元)                                            #>=2.5亿
+                # cash_flow.subtotal_operate_cash_inflow / indicator.adjusted_profit > 2.0,  # 经营活动现金流入小计/扣除非经常损益后的净利润(元)
+                # indicator.inc_return > 1.5,  # 净资产收益率(扣除非经常损益)(%)
+                # indicator.inc_net_profit_year_on_year > -15,  # 净利润同比增长率(%)
+                # valuation.code.in_(initial_list)
+                valuation.pb_ratio > 0,
+                valuation.pb_ratio < 1,
+                cash_flow.subtotal_operate_cash_inflow > 0,
+                indicator.adjusted_profit > 0,
+                cash_flow.subtotal_operate_cash_inflow / indicator.adjusted_profit > 2.0,
+                indicator.inc_return > 1.5,
+                indicator.inc_net_profit_year_on_year > -15,
+                valuation.code.in_(all_stocks)
+            ).order_by(
+                (indicator.roa / valuation.pb_ratio).desc()
+            ).limit(
+                self.max_select_count + 1
+            )
+        elif self.market_temperature == "warm":
+            q = query(
+                valuation.code,
+            ).filter(
+                # valuation.pb_ratio > 0,
+                # valuation.pb_ratio < 1,
+                # cash_flow.subtotal_operate_cash_inflow > 0,
+                # indicator.adjusted_profit > 2.5e8,  # 扣除非经常损益后的净利润(元)                          #>=2.5亿
+                # income.operating_revenue > 10e8,  # 营业收入(元)                                          #>=10亿
+                # income.net_profit > 2.5e8,  # 净利润(元)                                            #>=2.5亿
+                # cash_flow.subtotal_operate_cash_inflow / indicator.adjusted_profit > 1.0,
+                # indicator.inc_return > 2.0,
+                # indicator.inc_net_profit_year_on_year > 0,
+                # valuation.code.in_(initial_list)
+
+                valuation.pb_ratio > 0,
+                valuation.pb_ratio < 1,
+                cash_flow.subtotal_operate_cash_inflow > 0,
+                indicator.adjusted_profit > 0,
+                cash_flow.subtotal_operate_cash_inflow / indicator.adjusted_profit > 1.0,
+                indicator.inc_return > 2.0,
+                indicator.inc_net_profit_year_on_year > 0,
+                valuation.code.in_(all_stocks)
+            ).order_by(
+                (indicator.roa / valuation.pb_ratio).desc()
+            ).limit(
+                self.max_select_count + 1
+            )
+        elif self.market_temperature == "hot":
+            q = query(
+                valuation.code,
+            ).filter(
+                # valuation.pb_ratio > 3,
+                # cash_flow.subtotal_operate_cash_inflow > 0,
+                # indicator.adjusted_profit > 2.5e8,  # 扣除非经常损益后的净利润(元)                          #>=2.5亿
+                # income.operating_revenue > 10e8,  # 营业收入(元)                                          #>=10亿
+                # income.net_profit > 2.5e8,  # 净利润(元)                                            #>=2.5亿
+                # cash_flow.subtotal_operate_cash_inflow / indicator.adjusted_profit > 0.5,
+                # indicator.inc_return > 3.0,
+                # indicator.inc_net_profit_year_on_year > 20,
+                # valuation.code.in_(initial_list)
+                valuation.pb_ratio > 3,
+                cash_flow.subtotal_operate_cash_inflow > 0,
+                indicator.adjusted_profit > 0,
+                cash_flow.subtotal_operate_cash_inflow / indicator.adjusted_profit > 0.5,
+                indicator.inc_return > 3.0,
+                indicator.inc_net_profit_year_on_year > 20,
+                valuation.code.in_(all_stocks)
+            ).order_by(
+                indicator.roa.desc()
+            ).limit(
+                self.max_select_count + 1
+            )
+
+        check_out_lists = list(get_fundamentals(q).code)
+        return check_out_lists
+
+    def SMALL(self, context):
+        all_stocks1 = self.S_stocks
+        all_stocks2 = self.S_stocks
+
+        q = query(valuation.code, valuation.market_cap).filter(valuation.code.in_(all_stocks1),
+                                                               valuation.market_cap.between(5, 30)).order_by(
+            valuation.market_cap.asc())
+        df_fun = get_fundamentals(q)[:50]
+        final_list_1 = list(df_fun.code)
+
+        # 国九更新：过滤近一年净利润为负且营业收入小于1亿的
+        # 国九更新：过滤近一年期末净资产为负的 (经查询没有为负数的，所以直接pass这条)
+        # 国九更新：过滤近一年审计建议无法出具或者为负面建议的 (经过净利润等筛选，审计意见几乎不会存在异常)
+        q = query(
+            valuation.code,
+            valuation.market_cap,  # 总市值 circulating_market_cap/market_cap
+            income.np_parent_company_owners,  # 归属于母公司所有者的净利润
+            income.net_profit,  # 净利润
+            income.operating_revenue  # 营业收入
+            # security_indicator.net_assets
+        ).filter(
+            valuation.code.in_(all_stocks2),
+            valuation.market_cap.between(5, 30),
+            income.np_parent_company_owners > 0,
+            income.net_profit > 0,
+            income.operating_revenue > 1e8
+        ).order_by(valuation.market_cap.asc()).limit(50)
+
+        df = get_fundamentals(q)
+
+        final_list_2 = list(df.code)
+        last_prices = history(1, unit='1d', field='close', security_list=final_list_2)
+        # 过滤价格低于最高价50元/股的股票  ｜  再持仓列表中的股票
+        final_list_2 = [stock for stock in final_list_2 if
+                        stock in self.hold_list or last_prices[stock][-1] <= self.highest]
+
+        target_list = list(dict.fromkeys(final_list_1 + final_list_2))
+        target_list = target_list[:self.max_select_count * 3]
+        final_list = get_fundamentals(query(
+            valuation.code,
+            indicator.roe,
+            indicator.roa,
+        ).filter(
+            valuation.code.in_(target_list),
+            # valuation.pb_ratio<1
+        ).order_by(
+            valuation.market_cap.asc()
+        )).set_index('code').index.tolist()
+        return final_list
+
+    ####
+    def fun_delNewShare(self, context, equity, deltaday):
+        deltaDate = context.current_dt.date() - dt.timedelta(deltaday)
+        tmpList = []
+        for stock in equity:
+            if get_security_info(stock).start_date < deltaDate:
+                tmpList.append(stock)
+        return tmpList
---
+++ 分仓策略_3.py
@@ -0,0 +1,134 @@
+'''
+多策略分子账户并行
+用到的策略：
+蚂蚁量化,东哥：白马股攻防转换策略（dxw策略）
+linlin2018，ZLH：低波全天候策略（外盘ETF策略）
+@荒唐的方糖大佬:国九条小市值（dxw）（还可以改进）
+'''
+
+# 导入函数库
+#-*- coding: utf-8 -*-
+# 如果你的文件包含中文, 请在文件的第一行使用上面的语句指定你的文件编码
+
+# 用到策略及数据相关API请加入下面的语句(如果要兼容研究使用可以使用 try except导入
+import datetime
+import math
+import numpy as np
+import pandas as pd
+import pickle
+import requests
+import talib
+
+from kuanke.user_space_api import *
+from kuanke.wizard import *
+from jqdata import *
+from jqfactor import *
+from jqlib.technical_analysis import *
+from scipy.linalg import inv
+from prettytable import PrettyTable
+from WPETF_Strategy import WPETF_Strategy
+from XSZ_GJT_Strategy import XSZ_GJT_Strategy
+from DXW_Strategy import DXW_Strategy
+
+import warnings
+from datetime import date as dt
+
+
+# 初始化函数，设定基准等等
+def initialize(context):
+    log.warn('--initialize函数(只运行一次)--',
+             str(context.current_dt.date()) + ' ' + str(context.current_dt.time()))
+    # 设定沪深300作为基准
+    set_benchmark('000300.XSHG')
+    # 开启动态复权模式(真实价格)
+    set_option('use_real_price', True)
+    # 过滤掉order系列API产生的比error级别低的log
+    log.set_level('order', 'error')
+    # 关闭未来函数
+    set_option('avoid_future_data', True)
+
+    ### 股票相关设定 ###
+    # 股票类每笔交易时的手续费是：买入时佣金万分之三，卖出时佣金万分之三加千分之一印花税, 每笔交易佣金最低扣5块钱
+    set_order_cost(OrderCost(close_tax=0.001, open_commission=0.0001, close_commission=0.0001, min_commission=0),
+                   type='stock')
+
+    # 为股票设定滑点为百分比滑点
+    set_slippage(PriceRelatedSlippage(0.01), type='stock')
+
+    # 临时变量
+
+    # 持久变量
+    g.strategys = {}
+    # 子账户 分仓
+    g.portfolio_value_proportion = [1, 0, 0]
+
+    # 创建策略实例
+    # 初始化策略子账户 subportfolios
+    set_subportfolios([
+        SubPortfolioConfig(context.portfolio.starting_cash * g.portfolio_value_proportion[0], 'stock'),
+        SubPortfolioConfig(context.portfolio.starting_cash * g.portfolio_value_proportion[1], 'stock'),
+        SubPortfolioConfig(context.portfolio.starting_cash * g.portfolio_value_proportion[2], 'stock'),
+    ])
+
+
+    # 是否发送微信消息，回测环境不发送，模拟环境发送
+    context.is_send_wx_message = 0
+    params = {
+        'max_hold_count': 10,  # 最大持股数
+        'max_select_count': 30,  # 最大输出选股数
+    }
+    dxw_strategy = DXW_Strategy(context, subportfolio_index=0, name='大小外综合策略', params=params)
+    g.strategys[dxw_strategy.name] = dxw_strategy
+
+
+    if g.portfolio_value_proportion[0] > 0:
+        # 准备工作
+        run_daily(dxw_day_prepare, time='7:30')
+        # 选择大小外的其中一个
+        run_monthly(dxw_singal, 1, time='08:00')
+        # 选股
+        run_weekly(dxw_select, 1, time='08:30')
+        # 空仓/止损
+        # run_daily(dxw_open_market, time='9:30')
+        # 补仓卖出
+        run_weekly(dxw_adjust, 1, time='9:30')
+        # run_daily(dxw_sell_when_highlimit_open, time='11:27')
+        # 非涨停出售
+        # run_daily(dxw_sell_when_highlimit_open, time='14:00')
+        # run_daily(dxw_sell_when_highlimit_open, time='14:50')
+        # 补仓买入
+        run_daily(dxw_append_buy_stock, time='14:51')
+        # 收盘
+        run_daily(dxw_after_market_close, 'after_close')
+
+
+def dxw_day_prepare(context):
+    g.strategys['大小外综合策略'].day_prepare(context)
+
+
+def dxw_singal(context):
+    g.strategys['大小外综合策略'].singal(context)
+
+
+def dxw_select(context):
+    g.strategys['大小外综合策略'].select(context)
+
+
+def dxw_adjust(context):
+    g.strategys['大小外综合策略'].clear_append_buy_dict(context)
+    g.strategys['大小外综合策略'].adjustwithnoRM(context)
+
+
+def dxw_open_market(context):
+    g.strategys['大小外综合策略'].close_for_empty_month(context)
+    g.strategys['大小外综合策略'].close_for_stoplost(context)
+
+
+def dxw_sell_when_highlimit_open(context):
+    g.strategys['大小外综合策略'].sell_when_highlimit_open(context)
+
+def dxw_append_buy_stock(context):
+    g.strategys['大小外综合策略'].append_buy_dict(context)
+
+def dxw_after_market_close(context):
+    g.strategys['大小外综合策略'].after_market_close(context)
