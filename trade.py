52a9c07a08cf8a52d113aed8939614dbaab997b3
94390 <94390@qq.com>
2024-12-20 22:09:51 +0800

    提交

--- trade.py
+++ trade.py
@@ -1,13 +1,17 @@
 import easytrader
 
+# >pip install  easytrader   -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+# >pip install  pypiwin32   -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 user = easytrader.use('universal_client')
 
-user.connect(r'C:\同花顺软件\同花顺\xiadan.exe') # 类似 r'C:\htzqzyb2\xiadan.exe'
+# user.connect(r'C:\同花顺软件\同花顺\xiadan.exe') # 公司win
+user.connect(r'F:\soft\同花顺\xiadan.exe')  # 家庭win
 
-
-print("获取账户信息",user.balance)
-print("获取持仓情况",user.position)
+print("获取账户信息", user.balance)
+print("获取持仓情况", user.position)
 
 target = 'jq'  # joinquant
 follower = easytrader.follower(target)
 follower.login(user='18291880968', password='19970820Lwq.')
+url = 'https://www.joinquant.com/algorithm/live/index?backtestId=75bb5b6fc27373410da1d23932e984a1'
+follower.follow(user, url, trade_cmd_expire_seconds=100000000000, cmd_cache=False)
