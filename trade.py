2c039b29e7ab6e3bcfe535c1cf8f95ffe2fef7b0
94390 <94390@qq.com>
2024-12-20 22:28:30 +0800

    提交

--- trade.py
+++ trade.py
@@ -9,9 +9,11 @@ user.connect(r'F:\soft\同花顺\xiadan.exe')  # 家庭win
 
 print("获取账户信息", user.balance)
 print("获取持仓情况", user.position)
+print(user.buy('162411', price=0, amount=100))
+print(user.sell('162411', price=0, amount=100))
 
-target = 'jq'  # joinquant
-follower = easytrader.follower(target)
-follower.login(user='18291880968', password='19970820Lwq.')
-url = 'https://www.joinquant.com/algorithm/live/index?backtestId=75bb5b6fc27373410da1d23932e984a1'
-follower.follow(user, url, trade_cmd_expire_seconds=100000000000, cmd_cache=False)
+# target = 'jq'  # joinquant
+# follower = easytrader.follower(target)
+# follower.login(user='18291880968', password='19970820Lwq.')
+# url = 'https://www.joinquant.com/algorithm/live/index?backtestId=75bb5b6fc27373410da1d23932e984a1'
+# follower.follow(user, url, trade_cmd_expire_seconds=100000000000, cmd_cache=False)
