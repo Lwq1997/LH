66de8ca02895be6991665854bbc84100af8f50fb
liwanqi.gold <liwanqi.gold@bytedance.com>
2025-03-09 20:07:48 +0800

    修正代码

--- 偷鸡摸狗/偷鸡摸狗+极简投资_V5_online.py
+++ 偷鸡摸狗/偷鸡摸狗+极简投资_V5_online.py
@@ -98,17 +98,17 @@ def after_code_changed(context):  # 输出运行时间
         run_daily(prepare_pj_strategy, "7:00")
         run_monthly(select_pj_strategy, 1, "8:00")  # 阅读完成，测试完成
         run_monthly(adjust_pj_strategy, 1, "9:40")
-        run_daily(pj_sell_when_highlimit_open, time='11:20')
-        run_daily(pj_sell_when_highlimit_open, time='14:50')
+        run_daily(jsg_sell_when_highlimit_open, time='11:27')
+        run_daily(jsg_sell_when_highlimit_open, time='14:55')
 
     # 微盘策略调仓设置
     if g.portfolio_value_proportion[2] > 0:
         run_daily(prepare_jsg_strategy, "7:00")
-        run_daily(jsg_open_market, "9:35")
+        run_daily(jsg_open_market, "9:30")
         run_weekly(select_jsg_strategy, 1, "8:00")  # 阅读完成，测试完成
         run_weekly(adjust_jsg_strategy, 1, "11:00")
-        run_daily(jsg_sell_when_highlimit_open, time='11:20')
-        run_daily(jsg_sell_when_highlimit_open, time='14:50')
+        run_daily(jsg_sell_when_highlimit_open, time='11:27')
+        run_daily(jsg_sell_when_highlimit_open, time='14:55')
 
     # 全天策略调仓设置
     if g.portfolio_value_proportion[3] > 0:
