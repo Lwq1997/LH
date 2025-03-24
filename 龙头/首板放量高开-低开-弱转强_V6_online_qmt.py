2f8609df53e6dd10bb93812052e16c4bb5442783
liwanqi.gold <liwanqi.gold@bytedance.com>
2025-03-25 00:54:49 +0800

    优化bug

--- 龙头/首板放量高开-低开-弱转强_V6_online_qmt.py
+++ 龙头/首板放量高开-低开-弱转强_V6_online_qmt.py
@@ -220,7 +220,7 @@ def initialize(context):
     # 持久变量
     g.strategys = {}
     # 子账户 分仓
-    g.portfolio_value_proportion = [0, 0, 0, 1]
+    g.portfolio_value_proportion = [0, 0, 0,0, 1]
 
     # 创建策略实例
     # 初始化策略子账户 subportfolios
@@ -229,6 +229,7 @@ def initialize(context):
         SubPortfolioConfig(context.portfolio.starting_cash * g.portfolio_value_proportion[1], 'stock'),
         SubPortfolioConfig(context.portfolio.starting_cash * g.portfolio_value_proportion[2], 'stock'),
         SubPortfolioConfig(context.portfolio.starting_cash * g.portfolio_value_proportion[3], 'stock'),
+        SubPortfolioConfig(context.portfolio.starting_cash * g.portfolio_value_proportion[4], 'stock'),
     ])
 
     # 是否发送微信消息，回测环境不发送，模拟环境发送
@@ -259,9 +260,15 @@ def initialize(context):
         'max_hold_count': 100,  # 最大持股数
         'max_select_count': 100,  # 最大输出选股数
     }
-    total_strategy = Strategy(context, subportfolio_index=3, name='统筹交易策略', params=params)
-    g.strategys[total_strategy.name] = total_strategy
+    ogt_strategy = OGT_Strategy(context, subportfolio_index=3, name='一进二', params=params)
+    g.strategys[ogt_strategy.name] = ogt_strategy
 
+    params = {
+        'max_hold_count': 100,  # 最大持股数
+        'max_select_count': 100,  # 最大输出选股数
+    }
+    total_strategy = Strategy(context, subportfolio_index=4, name='统筹交易策略', params=params)
+    g.strategys[total_strategy.name] = total_strategy
 
 # 模拟盘在每天的交易时间结束后会休眠，第二天开盘时会恢复，如果在恢复时发现代码已经发生了修改，则会在恢复时执行这个函数。 具体的使用场景：可以利用这个函数修改一些模拟盘的数据。
 def after_code_changed(context):  # 输出运行时间
@@ -274,7 +281,7 @@ def after_code_changed(context):  # 输出运行时间
 
     run_daily(prepare_stock_list, time='09:00')
 
-    if g.portfolio_value_proportion[3] > 0:
+    if g.portfolio_value_proportion[4] > 0:
         # 选股
         run_daily(total_select, time='09:27')
         run_daily(total_buy, time='09:28')
@@ -290,7 +297,7 @@ def prepare_stock_list(context):
     # 文本日期
     date = context.previous_date
 
-    date_2, date_1, date = get_trade_days(end_date=date, count=3)
+    date_3, date_2, date_1, date = get_trade_days(end_date=date, count=4)
 
     # 初始列表
     initial_list = utilstool.stockpool(context, is_filter_highlimit=False,
@@ -298,6 +305,8 @@ def prepare_stock_list(context):
 
     # 昨日涨停
     yes_hl_list = utilstool.get_hl_stock(context, initial_list, date)
+    # 前日涨停
+    yes_yes_hl_list = utilstool.get_hl_stock(context, initial_list, date_1)
     # 昨日曾涨停过（包含涨停+涨停炸板）
     hl0_list = utilstool.get_ever_hl_stock(context, initial_list, date)
     # 前日曾涨停过（包含涨停+涨停炸板）
@@ -316,12 +325,22 @@ def prepare_stock_list(context):
     context.yes_no_first_hl_list = [stock for stock in yes_hl_list if stock not in hl1_list]
 
     # 昨日曾涨停炸板
-    h1_list = utilstool.get_ever_hl_stock2(context, initial_list, date)
+    h0_list = utilstool.get_ever_hl_stock2(context, initial_list, date)
+    h1_list = utilstool.get_ever_hl_stock2(context, initial_list, date_1)
+    h2_list = utilstool.get_ever_hl_stock2(context, initial_list, date_2)
+    h3_list = utilstool.get_ever_hl_stock2(context, initial_list, date_3)
     # 上上个交易日涨停
     elements_to_remove2 = utilstool.get_hl_stock(context, initial_list, date_1)
 
     # 过滤上上个交易日涨停、曾涨停
-    context.yes_first_no_hl_list = [stock for stock in h1_list if stock not in elements_to_remove2]
+    context.yes_first_no_hl_list = [stock for stock in h0_list if stock not in elements_to_remove2]
+
+    two_hl_list = list(set(yes_hl_list) & set(yes_yes_hl_list))  # 取交集，确保两个交易日均涨停
+    early_two_remove = list(set(h2_list) | set(h3_list))  # 取并集，确保前2个交易日没有涨停过
+    early_three_remove = list(set(h1_list) | set(h2_list))  # 取并集，确保前2个交易日没有涨停过
+
+    context.two_hl_list = [stock for stock in two_hl_list if stock not in early_two_remove]
+    context.three_hl_list = [stock for stock in yes_hl_list if stock not in early_three_remove]
 
     if len(yes_hl_list) > 0 and len(hl0_list) > 0:
         log.info(
@@ -334,10 +353,12 @@ def total_select(context):
     g.strategys['弱转强'].select(context)
     g.strategys['首板高开'].select(context)
     g.strategys['首板低开'].select(context)
+    g.strategys['一进二'].select(context)
     total_stocks = set(
         g.strategys['弱转强'].select_list
         + g.strategys['首板高开'].select_list
         + g.strategys['首板低开'].select_list
+        + g.strategys['一进二'].select_list
     )
     g.strategys['统筹交易策略'].select_list = total_stocks
     if total_stocks:
@@ -2336,9 +2357,6 @@ class RZQ_Strategy_V3(Strategy):
 
         # 弱转强
         for s in yes_first_no_hl_list:
-            zyts = self.utilstool.calculate_zyts(context, s)
-
-            his_day = zyts if zyts > 4 else 4
             all_date = attribute_history(s, 4, '1d', fields=['close', 'volume', 'money', 'open'],
                                          skip_paused=True)
 
@@ -2478,8 +2496,8 @@ class SBDK_Strategy_V3(Strategy):
         if yes_no_first_hl_list:
             date = self.utilstool.transform_date(context, context.previous_date, 'str')
             # 计算相对位置
-            rpd = self.utilstool.get_relative_position_df(context, yes_no_first_hl_list, date, 60)
-            rpd = rpd[rpd['rp'] <= 0.5]
+            rpd = self.utilstool.get_relative_position_df(context, yes_no_first_hl_list, date, 120)
+            rpd = rpd[rpd['rp'] <= 0.4]
             stock_list = list(rpd.index)
 
             # 低开
@@ -2498,3 +2516,143 @@ class SBDK_Strategy_V3(Strategy):
         log.info('今日首板低开选股：' + ','.join('%s%s' % (s, get_security_info(s).display_name) for s in sbdk_stocks))
 
         return sbdk_stocks
+
+
+class OGT_Strategy(Strategy):
+    def __init__(self, context, subportfolio_index, name, params):
+        super().__init__(context, subportfolio_index, name, params)
+        self.n_days_limit_up_list = []
+
+    def select(self, context):
+        log.info(self.name, '--select函数--', str(context.current_dt.date()) + ' ' + str(context.current_dt.time()))
+
+        # 根据市场温度设置选股条件，选出股票
+        self.select_list = self.__get_rank(context)
+        # 编写操作计划
+        self.print_trade_plan(context, self.select_list)
+
+    def __get_rank(self, context):
+        log.info(self.name, '--get_rank函数--', str(context.current_dt.date()) + ' ' + str(context.current_dt.time()))
+
+        two_hl_list = context.two_hl_list
+        three_hl_list = context.three_hl_list
+        current_data = get_current_data()
+        log.info(self.name, '的选股底池--two_hl_list:', two_hl_list)
+        log.info(self.name, '的选股底池--three_hl_list:', three_hl_list)
+        ogt_stocks = []
+        date_now = context.current_dt.strftime("%Y-%m-%d")
+        mid_time1 = ' 09:15:00'
+        end_times1 = ' 09:26:00'
+        start = date_now + mid_time1
+        end = date_now + end_times1
+
+        # 首板高开/低开
+        for s in two_hl_list:
+            # 条件一：均价，金额，市值，换手率
+            prev_day_data = attribute_history(s, 1, '1d', fields=['close', 'volume', 'money'], skip_paused=True)
+            avg_price_increase_value = prev_day_data['money'][0] / prev_day_data['volume'][0] / prev_day_data['close'][
+                0] * 1.1 - 1
+            # 如果平均价格涨幅小于0.07或者前一个交易日的成交金额小于7亿或者大于20亿，则跳过
+            if avg_price_increase_value < 0.07 or prev_day_data['money'][0] < 7e8 or prev_day_data['money'][0] > 30e8:
+                continue
+
+            # 条件二: market_cap 总市值(亿元) > 70亿, 流通市值(亿元) < 300亿
+            turnover_ratio_data = get_valuation(s, start_date=context.previous_date, end_date=context.previous_date,
+                                                fields=['turnover_ratio', 'market_cap', 'circulating_market_cap'])
+            if turnover_ratio_data.empty or not (70 <= turnover_ratio_data['market_cap'][0] <= 520) or \
+                    turnover_ratio_data['circulating_market_cap'][0] > 300:
+                continue
+
+            yesterday_turnover_ratio = turnover_ratio_data['turnover_ratio'][0]
+            if yesterday_turnover_ratio < 10 or yesterday_turnover_ratio > 30:
+                continue
+
+            # 条件三：昨日涨停的成交量为近100日的最大成交量
+            # 获取昨日成交量
+            yesterday_volume = prev_day_data['volume'][0]
+
+            log.info(f'{s}昨日成交量{yesterday_volume}')
+            # 获取过去100个交易日的成交量
+            past_volume_data = attribute_history(s, 100, '1d', fields=['volume'], skip_paused=True)
+            if past_volume_data.empty:
+                continue
+            max_past_volume = past_volume_data['volume'].max()
+            log.info(f'{s}最大成交量{max_past_volume}')
+            if yesterday_volume < max_past_volume:
+                continue
+
+            # 条件四： 昨日收盘时封单金额需大于流通市值的2%
+            # 获取昨日收盘时的封单金额
+            # 使用 get_ticks 获取昨日最后一笔的盘口数据
+
+            edate = context.previous_date
+            end_time = str(edate) + ' ' + '15:00:00'
+            ticks = get_ticks(s, end_dt=end_time, count=1, fields=['time', 'a1_v', 'a1_p', 'b1_v', 'b1_p'], skip=False,
+                              df=True)
+            if len(ticks) == 0:
+                continue
+
+            bid_volume = ticks['b1_p'].iloc[0]
+            bid_price = ticks['b1_v'].iloc[0]
+            # 计算封单金额
+            order_amount = bid_volume * bid_price
+            # 获取流通市值
+            circulating_market_cap = turnover_ratio_data['circulating_market_cap'][0]
+            # 计算封单金额占流通市值的比例
+            order_ratio = order_amount / (circulating_market_cap * 100000000)
+            if order_ratio < 0.01:
+                continue
+
+            df = get_price(s, end_date=context.previous_date, frequency='daily', fields=['low', 'close', 'low_limit'],
+                           count=10,
+                           panel=False,
+                           fill_paused=False, skip_paused=False)
+            low_limit_count = len(df[df.close == df.low_limit])
+            if low_limit_count >= 1:
+                continue
+
+            # 将符合条件的股票添加到保存的股票列表中
+            ogt_stocks.append(s)
+        #
+        # for s in three_hl_list:
+        #     # 过滤前面三天涨幅超过18%的票
+        #     price_data = attribute_history(s, 4, '1d', fields=['close'], skip_paused=True)
+        #     increase_ratio = (price_data['close'][-1] - price_data['close'][0]) / price_data['close'][0]
+        #     if len(price_data) < 4 or increase_ratio > 0.18:
+        #         continue
+        #
+        #     # 条件一：均价，金额，市值，换手率
+        #     prev_day_data = attribute_history(s, 1, '1d', fields=['close', 'volume', 'money'], skip_paused=True)
+        #     avg_price_increase_value = prev_day_data['money'][0] / prev_day_data['volume'][0] / prev_day_data['close'][
+        #         0] * 1.1 - 1
+        #     # 如果平均价格涨幅小于0.07或者前一个交易日的成交金额小于7亿或者大于20亿，则跳过
+        #     if avg_price_increase_value < 0.07 or prev_day_data['money'][0] < 7e8 or prev_day_data['money'][0] > 20e8:
+        #         continue
+        #     # 如果换手率为空或者市值小于70，则跳过
+        #     turnover_ratio_data = get_valuation(s, start_date=context.previous_date, end_date=context.previous_date,
+        #                                         fields=['turnover_ratio', 'market_cap', 'circulating_market_cap'])
+        #     if turnover_ratio_data.empty or turnover_ratio_data['market_cap'][0] < 70 or \
+        #             turnover_ratio_data['circulating_market_cap'][0] > 300:
+        #         continue
+        #     # 如果近期有跌停，则跳过
+        #     df = get_price(s, end_date=context.previous_date, frequency='daily', fields=['low', 'close', 'low_limit'],
+        #                    count=10,
+        #                    panel=False,
+        #                    fill_paused=False, skip_paused=False)
+        #     low_limit_count = len(df[df.close == df.low_limit])
+        #     if low_limit_count >= 1:
+        #         continue
+        #
+        #     # 条件二：左压
+        #     zyts = self.utilstool.calculate_zyts(context, s)
+        #     volume_data = attribute_history(s, zyts, '1d', fields=['volume'], skip_paused=True)
+        #     if len(volume_data) < 2 or volume_data['volume'][-1] <= max(volume_data['volume'][:-1]) * 0.90:
+        #         continue
+        #
+        #     # 将符合条件的股票添加到保存的股票列表中
+        #     ogt_stocks.append(s)
+
+        ogt_stocks = list(set(ogt_stocks))
+        log.info('今日一进二选股：' + ','.join('%s%s' % (s, get_security_info(s).display_name) for s in ogt_stocks))
+
+        return ogt_stocks
